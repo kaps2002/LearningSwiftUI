427b97c28fd5696da3517f085a7450e98a43b742
kaps2002 <karpandey2002@gmail.com>
2024-03-18 06:38:19 +0530

    Added the Image assets and refactored th code.

--- Landmarks/Landmarks/Model/Landmark.swift
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import MapKit
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
--- Landmarks/Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct LandmarkRow: View {
+    
+    var landmark: Landmark
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    LandmarkRow()
+    List(landmarks) { landmark in
+        LandmarkRow(landmark: landmark)
+    }
 }
