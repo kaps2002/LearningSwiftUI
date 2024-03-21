8b6b1f9a5e590d5a396d130510c634115cbeb75e
kaps2002 <karpandey2002@gmail.com>
2024-03-21 11:50:11 +0530

    Added thee Tabs in app

--- Landmarks/Landmarks/Views/ContentView.swift
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedTab = Tab.featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        LandmarkList()
+        TabView(selection: $selectedTab) {
+            CategoryHome()
+                .tabItem {
+                    Label(
+                        title: {
+                            Text("Featured")
+                        },
+                        icon: {
+                            Image(systemName: "star.fill")
+                        }
+                    )
+                }
+                .tag(Tab.featured)
+            LandmarkList()
+                .tabItem {
+                    Label(
+                        title: {
+                            Text("List")
+                        },
+                        icon: {
+                            Image(systemName: "list.bullet")
+                        }
+                    )
+                }
+                .tag(Tab.list)
+        }
         
     }
 }
