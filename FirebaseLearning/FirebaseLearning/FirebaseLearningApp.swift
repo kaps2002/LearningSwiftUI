3b29352222dcded741be5a04e87c76b9450da221
kaps2002 <karpandey2002@gmail.com>
2024-05-06 15:26:05 +0530

    Started learning firebase with ios

---
+++ FirebaseLearning/FirebaseLearning/FirebaseLearningApp.swift
@@ -0,0 +1,23 @@
+//
+//  FirebaseLearningApp.swift
+//  FirebaseLearning
+//
+//  Created by Roro on 06/05/24.
+//
+
+import SwiftUI
+import Firebase
+@main
+struct FirebaseLearningApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+        print("Configured the firebase")
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
